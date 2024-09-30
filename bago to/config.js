14757245b5aab86ab8f3818e3a248f96851c54a4
Troy Ceralde <117468396+TroyCeralde@users.noreply.github.com>
2024-10-01 00:30:19 +0800

    Add files via upload

---
+++ bago to/config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    titleWeb: "I LOVE YOU!!!!",
+    introTitle: 'Can we go out?',
+    introDesc: `i know its so fast but can i get you a date? wrong grammar yan alam ko
+    pero pwede nga ba???`,
+    btnIntro: '^^CLICK THIS^^',
+    title: 'nice AHHAHA since you click it so can we??? 🥰',
+    desc: 'can wee?? ill promise ill make it worth itt ',
+    btnYes: 'yes',
+    btnNo: 'no',
+    question: 'oh ayan nagyes ka ha saan tayo kakain? HAHAH and what dou want?',
+    btnReply: 'thank youuu',
+    reply: '',
+    mess: 'promise hinding hindi ka magsisi and ill take my best shot',
+    messDesc: 'see you ',
+    btnAccept: 'chat na HAHAHA click this',
+    messLink: 'https://www.facebook.com/wesleyjester.abalos' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+}
